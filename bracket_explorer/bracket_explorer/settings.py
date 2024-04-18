896fe6c9c3f7ba430291d5d8055a96bb8343de7b
Chathan Driehuys <chathan@driehuys.com>
2024-04-18 00:50:22 +0000

    Add Postgres database configuration

--- bracket_explorer/bracket_explorer/settings.py
+++ bracket_explorer/bracket_explorer/settings.py
@@ -92,12 +92,24 @@ WSGI_APPLICATION = "bracket_explorer.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if env_bool("BE_DEV_EPHEMERAL_DB"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": ":memory:",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("BE_DB_NAME"),
+            "USER": os.getenv("BE_DB_USER"),
+            "PASSWORD": os.getenv("BE_DB_PASSWORD"),
+            "HOST": os.getenv("BE_DB_HOST"),
+            "PORT": os.getenv("BE_DB_PORT"),
+        }
+    }
 
 
 # Password validation
